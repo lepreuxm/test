775c3606b976d86659a2f8a925a3614acf69313e
Romain Medici <medici.romain.lgm@gmail.com>
2021-03-03 10:28:19 +0100

    Ajout d'un fichier python pour test git

---
+++ fichier1_git_test.py
@@ -0,0 +1,17 @@
+# coding: UTF-8
+"""
+Script: fichier1_git_test/fichier1_git_test
+Création: rmedici, le 03/03/2021
+"""
+
+
+# Imports
+# Fonctions
+# Programme principal
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+# Fin
